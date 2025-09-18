6e7f104d1fde3b9121a3d732fab4053bc65504a2
Мария Юсупова <yusupova.maria25@yandex.ru>
2025-09-18 22:08:46 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask
 app = Flask (__name__)
 
 @app.route("/")
-def start ():
+@app.route("/web")
+def web():
     return """<!doctype html>
         <!html>
              <body>
                  <hl>web-сервер на flask</hl>
+                 <a href="/author">author</a>
              </body>
         <!html>"""
+
+@app.route("/author")
+def author():
+    name = "Юсупова Мария Леонидовна"
+    group = "ФБИ-34"
+    faculty = "ФБ"
+    
+    return """<!doctype html> 
+        <html>
+            <body>
+                <p>Студент: """+ name + """</p>
+                <p>Группа: """ + group + """</p>
+                </p>Факультет: """+ faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+         </html>"""
