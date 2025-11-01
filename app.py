8b8a30dfa81ac34647c261c6ab58b99175cc28e4
Мария Юсупова <yusupova.maria25@yandex.ru>
2025-11-01 09:02:50 +0700

    Имя студента

--- app.py
+++ app.py
@@ -312,4 +312,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Петров Иван'
+    return render_template('example.html', name=name)
