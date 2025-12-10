2aa80388755bd6a93c8f7916e0df44b1ff086baa
Мария Юсупова <yusupova.maria25@yandex.ru>
2025-12-11 01:30:48 +0700

    Деление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for  # ← ДОБАВЬ url_for
+from flask import Flask, render_template, url_for 
 import datetime
 from lab1 import lab1
 from lab2 import lab2
