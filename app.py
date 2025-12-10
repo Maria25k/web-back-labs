5df41a3facf52d11de27d52b49924254e7c803c9
Мария Юсупова <yusupova.maria25@yandex.ru>
2025-12-11 05:37:39 +0700

    Лабораторная работа 5

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3 
 from lab4 import lab4
+from lab5 import lab5
 
 app = Flask(__name__)
 app.secret_key = 'секретно-секретный секрет'
@@ -11,7 +12,8 @@ app.secret_key = 'секретно-секретный секрет'
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3) 
-app.register_blueprint(lab4) 
+app.register_blueprint(lab4)
+app.register_blueprint(lab5)  
 
 @app.route("/")
 @app.route("/index")
@@ -29,7 +31,8 @@ def index():
             <li><a href="/lab1/">Первая лабораторная</a></li>
             <li><a href="/lab2/">Вторая лабораторная</a></li>        
             <li><a href="/lab3/">Третья лабораторная</a></li> 
-            <li><a href="/lab4/">Четвертая лабораторная</a></li>             
+            <li><a href="/lab4/">Четвертая лабораторная</a></li> 
+            <li><a href="/lab4/">Пятая лабораторная</a></li>             
         </ul>
         <footer>
             <p>Юсупова Мария Леонидовна, ФБИ-34, 3 курс, 2025 год</p>
