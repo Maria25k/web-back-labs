301de3bbc8fcdfeaba7dac57e8a6678d4bd38780
Мария Юсупова <yusupova.maria25@yandex.ru>
2026-01-19 16:58:45 +0700

    лабораторная 8

--- app.py
+++ app.py
@@ -1,38 +1,74 @@
-from flask import Flask, render_template, url_for 
+from flask import Flask, render_template, url_for
+from flask_login import LoginManager
 import datetime
 import os
+from os import path
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 from lab1 import lab1
 from lab2 import lab2
-from lab3 import lab3 
+from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 from rgz import rgz
-
+from db import db
+from db.models import users
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный-секрет')
 app.config['DB_TYPE'] = os.environ.get('DB_TYPE', 'postgres')
 
+# Настройка соединения с БД
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = os.environ.get('DB_NAME', 'ivan_ivanov_orm')
+    db_user = os.environ.get('DB_USER', 'ivan_ivanov_orm')
+    db_password = os.environ.get('DB_PASSWORD', '123')
+    host_ip = os.environ.get('DB_HOST', '127.0.0.1')
+    host_port = os.environ.get('DB_PORT', '5432')
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "ivan_ivanov_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Инициализация БД
+db.init_app(app)
+
+# Настройка Flask-Login
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
+
+# Регистрация Blueprint'ов
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
-app.register_blueprint(lab3) 
+app.register_blueprint(lab3)
 app.register_blueprint(lab4)
-app.register_blueprint(lab5)  
-app.register_blueprint(lab6)  
-app.register_blueprint(lab7)  
+app.register_blueprint(lab5)
+app.register_blueprint(lab6)
+app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 app.register_blueprint(rgz)
+
+# Создание таблиц при первом запуске
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 @app.route("/index")
 def index():
-
     return """
     <!DOCTYPE html>
     <html>
@@ -50,7 +86,6 @@ def index():
             <li><a href="/lab6/">Шестая лабораторная</a></li>
             <li><a href="/lab7/">Седьмая лабораторная</a></li>
             <li><a href="/lab8/">Восьмая лабораторная</a></li>
-            <li><a href="/lab9/">Девятая лабораторная</a></li>
             <li><a href="/rgz/">Расчетно графическое задание</a></li>             
         </ul>
         <footer>
--- lab8.py
+++ lab8.py
@@ -1,278 +1,194 @@
-from flask import Blueprint, render_template, request, session, redirect, current_app
-import psycopg2
-import sqlite3
-from psycopg2.extras import RealDictCursor
+from flask import Blueprint, render_template, request, redirect, session, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 from os import path
+from db import db
+from db.models import users, articles
+from sqlalchemy import or_
 
-lab5 = Blueprint('lab5', __name__)
+lab8 = Blueprint('lab8', __name__)
 
-def db_connect():
-    """Подключение к БД в зависимости от типа"""
-    db_type = current_app.config.get('DB_TYPE', 'postgres')
-    
-    if db_type == 'postgres':
-        # PostgreSQL
-        conn = psycopg2.connect(
-            host='127.0.0.1',
-            database='maria_yusupova_knowledge_base',
-            user='maria_yusupova_knowledge_base',
-            password='123'
-        )
-        cur = conn.cursor(cursor_factory=RealDictCursor)
-    else:
-        # SQLite
-        dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "database.db")
-        conn = sqlite3.connect(db_path)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-    
-    return conn, cur
+# Настройка соединения с БД будет в app.py
 
-def db_close(conn, cur):
-    """Закрытие соединения с БД"""
-    conn.commit()
-    cur.close()
-    conn.close()
-
-@lab5.route('/lab5')
-@lab5.route('/lab5/')
-def main():
-    login = session.get('login')
-    return render_template('lab5/lab5.html', login=login)
-
-@lab5.route('/lab5/register', methods=['GET', 'POST'])
+# Регистрация
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        return render_template('lab5/register.html')
-
-    login = request.form.get('login')
-    password = request.form.get('password')
-    real_name = request.form.get('real_name', '')  
-
-    if not (login and password):
-        return render_template('lab5/register.html', error='Заполните все поля')
-
-    conn, cur = db_connect()
+        return render_template('lab8/register.html')
     
-    cur.execute("SELECT login FROM users WHERE login=%s", (login,))
-    if cur.fetchone():
-        db_close(conn, cur)
-        return render_template('lab5/register.html', error='Такой пользователь уже существует')
-
-    password_hash = generate_password_hash(password)
-    cur.execute("INSERT INTO users (login, password, real_name) VALUES (%s, %s, %s)", 
-            (login, password_hash, real_name))
-    db_close(conn, cur)
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
     
-    return render_template('lab5/success.html', login=login)
+    # Проверка на пустые значения
+    if not login_form or not password_form:
+        return render_template('lab8/register.html', 
+                             error='Логин и пароль не могут быть пустыми')
+    
+    # Проверка существования пользователя
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', 
+                             error='Такой пользователь уже существует')
+    
+    # Создание нового пользователя
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    # Автоматический логин после регистрации
+    login_user(new_user, remember=False)
+    
+    return redirect('/lab8/')
 
-@lab5.route('/lab5/login', methods=['GET', 'POST'])
+# Авторизация
+@lab8.route('/lab8/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('lab5/login.html')
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+    remember_me = request.form.get('remember') == 'on'
+    
+    # Проверка на пустые значения
+    if not login_form or not password_form:
+        return render_template('lab8/login.html', 
+                             error='Логин и пароль не могут быть пустыми')
+    
+    # Поиск пользователя в БД
+    user = users.query.filter_by(login=login_form).first()
+    
+    if user and check_password_hash(user.password, password_form):
+        login_user(user, remember=remember_me)
+        next_page = request.args.get('next')
+        return redirect(next_page or '/lab8/')
+    
+    return render_template('lab8/login.html', 
+                         error='Ошибка входа: неверный логин или пароль')
 
-    login = request.form.get('login')
-    password = request.form.get('password')
-
-    if not (login and password):
-        return render_template('lab5/login.html', error='Заполните все поля')
-
-    conn, cur = db_connect()
-    cur.execute("SELECT * FROM users WHERE login=%s", (login,))
-    user = cur.fetchone()
-    db_close(conn, cur)
-
-    if not user or not check_password_hash(user['password'], password):
-        return render_template('lab5/login.html', error='Неверный логин или пароль')
-
-    session['login'] = login
-    return redirect('/lab5')
-
-@lab5.route('/lab5/logout')
+# Логаут
+@lab8.route('/lab8/logout')
+@login_required
 def logout():
-    session.pop('login', None)
-    return redirect('/lab5')
+    logout_user()
+    return redirect('/lab8/')
 
-@lab5.route('/lab5/create', methods=['GET', 'POST'])
-def create():
-    login = session.get('login')
-    if not login:
-        return redirect('/lab5/login')
+# Главная страница lab8
+@lab8.route('/lab8/')
+def index():
+    username = current_user.login if current_user.is_authenticated else 'anonymous'
+    return render_template('lab8/index.html', username=username)
 
+# Список статей
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    # Получаем статьи текущего пользователя
+    user_articles = articles.query.filter_by(login_id=current_user.id).all()
+    # Получаем публичные статьи других пользователей
+    public_articles = articles.query.filter(
+        articles.is_public == True, 
+        articles.login_id != current_user.id
+    ).all()
+    return render_template('lab8/articles.html', 
+                         user_articles=user_articles,
+                         public_articles=public_articles)
+
+# Создание статьи
+@lab8.route('/lab8/create', methods=['GET', 'POST'])
+@login_required
+def create_article():
     if request.method == 'GET':
-        return render_template('lab5/create_article.html')
-
+        return render_template('lab8/create.html')
+    
     title = request.form.get('title')
     article_text = request.form.get('article_text')
-
-    # ВАЛИДАЦИЯ: проверка на пустые поля и пробелы
+    is_favorite = request.form.get('is_favorite') == 'on'
+    is_public = request.form.get('is_public') == 'on'
+    
     if not title or not article_text:
-        return render_template('lab5/create_article.html', error='Название и текст статьи не могут быть пустыми')
-    if len(title.strip()) == 0 or len(article_text.strip()) == 0:
-        return render_template('lab5/create_article.html', error='Название и текст статьи не могут состоять только из пробелов')
-
-    conn, cur = db_connect()
-
-    cur.execute("SELECT id FROM users WHERE login=%s", (login,))
-    user = cur.fetchone()
-    user_id = user['id']
-
-    cur.execute("""
-        INSERT INTO articles (user_id, title, article_text, is_favorite, is_public, likes) 
-        VALUES (%s, %s, %s, %s, %s, %s)
-    """, (user_id, title, article_text, False, False, 0))
-
-    db_close(conn, cur)
-    return redirect('/lab5')
-
-@lab5.route('/lab5/list')
-def articles_list():
-    login = session.get('login')
-    if not login:
-        return redirect('/lab5/login')
-
-    conn, cur = db_connect()
-
-    cur.execute("SELECT id FROM users WHERE login=%s", (login,))
-    user = cur.fetchone()
+        return render_template('lab8/create.html', 
+                             error='Заголовок и текст статьи не могут быть пустыми')
     
-    if not user:  
-        db_close(conn, cur)
-        return redirect('/lab5') 
-    user_id = user['id']  
+    new_article = articles(
+        login_id=current_user.id,
+        title=title,
+        article_text=article_text,
+        is_favorite=is_favorite,
+        is_public=is_public,
+        likes=0
+    )
+    
+    db.session.add(new_article)
+    db.session.commit()
+    
+    return redirect('/lab8/articles/')
 
-    cur.execute("SELECT * FROM articles WHERE user_id=%s ORDER BY is_favorite DESC, id DESC", (user_id,))
-    articles = cur.fetchall()
-    db_close(conn, cur)
-    return render_template('lab5/articles.html', articles=articles, login=login)
-@lab5.route('/lab5/edit/<int:article_id>', methods=['GET', 'POST'])
+# Редактирование статьи
+@lab8.route('/lab8/edit/<int:article_id>', methods=['GET', 'POST'])
+@login_required
 def edit_article(article_id):
-    login = session.get('login')
-    if not login:
-        return redirect('/lab5/login')
-
-    conn, cur = db_connect()
+    article = articles.query.filter_by(id=article_id, login_id=current_user.id).first()
     
-    cur.execute("SELECT a.*, u.login FROM articles a JOIN users u ON a.user_id = u.id WHERE a.id=%s", (article_id,))
-    article = cur.fetchone()
+    if not article:
+        return redirect('/lab8/articles/')
     
-    if not article or article['login'] != login:
-        db_close(conn, cur)
-        return redirect('/lab5/list')
-
     if request.method == 'GET':
-        db_close(conn, cur)
-        return render_template('lab5/edit_article.html', article=article)
-
+        return render_template('lab8/edit.html', article=article)
+    
     title = request.form.get('title')
     article_text = request.form.get('article_text')
+    is_favorite = request.form.get('is_favorite') == 'on'
+    is_public = request.form.get('is_public') == 'on'
+    
+    if not title or not article_text:
+        return render_template('lab8/edit.html', 
+                             article=article,
+                             error='Заголовок и текст статьи не могут быть пустыми')
+    
+    article.title = title
+    article.article_text = article_text
+    article.is_favorite = is_favorite
+    article.is_public = is_public
+    
+    db.session.commit()
+    return redirect('/lab8/articles/')
 
-    if not (title and article_text):
-        db_close(conn, cur)
-        return render_template('lab5/edit_article.html', 
-                              article=article, 
-                              error='Заполните все поля')
-
-    cur.execute("UPDATE articles SET title=%s, article_text=%s WHERE id=%s", 
-                (title, article_text, article_id))
-    db_close(conn, cur)
-    return redirect('/lab5/list')
-
-@lab5.route('/lab5/delete/<int:article_id>')
+# Удаление статьи
+@lab8.route('/lab8/delete/<int:article_id>')
+@login_required
 def delete_article(article_id):
-    login = session.get('login')
-    if not login:
-        return redirect('/lab5/login')
+    article = articles.query.filter_by(id=article_id, login_id=current_user.id).first()
+    
+    if article:
+        db.session.delete(article)
+        db.session.commit()
+    
+    return redirect('/lab8/articles/')
 
-    conn, cur = db_connect()
+# Поиск статей
+@lab8.route('/lab8/search')
+@login_required
+def search_articles():
+    query = request.args.get('q', '')
     
-    cur.execute("SELECT a.*, u.login FROM articles a JOIN users u ON a.user_id = u.id WHERE a.id=%s", (article_id,))
-    article = cur.fetchone()
+    if not query:
+        return redirect('/lab8/articles/')
     
-    if article and article['login'] == login:
-        cur.execute("DELETE FROM articles WHERE id=%s", (article_id,))
+    # Поиск в своих статьях и публичных статьях
+    search_results = articles.query.filter(
+        or_(
+            articles.login_id == current_user.id,
+            articles.is_public == True
+        ),
+        or_(
+            articles.title.ilike(f'%{query}%'),
+            articles.article_text.ilike(f'%{query}%')
+        )
+    ).all()
     
-    db_close(conn, cur)
-    return redirect('/lab5/list')
-
-@lab5.route('/lab5/users')
-def all_users():
-    login = session.get('login')
-    if not login:
-        return redirect('/lab5/login')
-
-    conn, cur = db_connect()
-    cur.execute("SELECT login, real_name FROM users ORDER BY login")
-    users = cur.fetchall()
-    db_close(conn, cur)
-    
-    return render_template('lab5/all_users.html', users=users)
-
-@lab5.route('/lab5/profile', methods=['GET', 'POST'])
-def profile():
-    login = session.get('login')
-    if not login:
-        return redirect('/lab5/login')
-
-    conn, cur = db_connect()
-    
-    if request.method == 'GET':
-        cur.execute("SELECT real_name FROM users WHERE login=%s", (login,))
-        user = cur.fetchone()
-        db_close(conn, cur)
-        return render_template('lab5/profile.html', real_name=user['real_name'])
-
-    # POST - сохранение изменений
-    new_real_name = request.form.get('real_name')
-    current_password = request.form.get('current_password')
-    new_password = request.form.get('new_password')
-    confirm_password = request.form.get('confirm_password')
-
-    cur.execute("SELECT password FROM users WHERE login=%s", (login,))
-    user = cur.fetchone()
-    
-    error = None
-    
-    if new_password:
-        if not check_password_hash(user['password'], current_password):
-            error = 'Текущий пароль неверен'
-        elif new_password != confirm_password:
-            error = 'Новый пароль и подтверждение не совпадают'
-        elif len(new_password) < 3:
-            error = 'Новый пароль слишком короткий'
-        else:
-            new_password_hash = generate_password_hash(new_password)
-            cur.execute("UPDATE users SET password=%s WHERE login=%s", 
-                       (new_password_hash, login))
-    
-    if not error:
-        cur.execute("UPDATE users SET real_name=%s WHERE login=%s", 
-                   (new_real_name, login))
-    
-    db_close(conn, cur)
-    
-    if error:
-        return render_template('lab5/profile.html', 
-                              real_name=new_real_name, 
-                              error=error)
-    
-    return redirect('/lab5')
-
-@lab5.route('/lab5/public')
-def public_articles():
-    conn, cur = db_connect()
-    cur.execute("""
-        SELECT a.*, u.login as author 
-        FROM articles a 
-        JOIN users u ON a.user_id = u.id 
-        WHERE a.is_public = TRUE 
-        ORDER BY a.likes DESC
-    """)
-    articles = cur.fetchall()
-    db_close(conn, cur)
-    
-    return render_template('lab5/public_articles.html', 
-                          articles=articles, 
-                          login=session.get('login'))
+    return render_template('lab8/search.html', 
+                         query=query,
+                         results=search_results)
